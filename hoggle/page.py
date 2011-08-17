c5a2e7bc4288382912c4f354df72e69e28ed387d
Sir Valiance <sir@sirvaliance.com>
2011-08-16 22:32:17 -0500

    Updated page.py to PEP8

--- hoggle/page.py
+++ hoggle/page.py
@@ -9,31 +9,30 @@ template_header = """
 """
 
 template_footer = """
-	<div class="clear_40"></div>
-</div>	
+<div class="clear_40"></div>
+</div>
 {% end %}
 
 """
 
-class Page(object):
 
+class Page(object):
 
     def __init__(self, output_dir_name, markdown_file):
 
         filename = markdown_file.split('.md')[0]
 
-        page_name = str(output_dir_name.split('/')[-1]).title() + str(filename).title()
+        page_name = output_dir_name.split('/')[-1].title()
+        page_name += filename.title()
         page_name = page_name.replace('-', '')
 
         template_path = output_dir_name.split("/")
-        page_template_name = "%s/%s.html" % ("/".join(template_path), filename) 
-
+        page_template_name = "%s/%s.html" % ("/".join(template_path), filename)
 
         if filename == "index":
             page_handler = "/%s/" % (output_dir_name)
         else:
             page_handler = "/%s/%s/" % (output_dir_name, filename)
-
 
         self.name = page_name
         self.markdown_file = markdown_file
@@ -50,12 +49,9 @@ class Page(object):
 
         m = template_header + m + template_footer
 
-        # make this path specifiable by the user as cmd arg
-        
-
         # Change to a more readable format
-        output_path = output_directory + "templates/%s/%s.html" % (self.output_dir_name, self.filename)
+        output_path = output_directory + "templates/%s/%s.html"
+        output_path = output_path % (self.output_dir_name, self.filename)
         output = open(output_path, 'w')
         output.write(m)
         output.close()
-
